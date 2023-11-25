c6a025693516461b583018e06bdee19a4252a883
Muyang Li <Limuyang9612@163.com>
2023-11-24 22:54:13 -0800

    fix bug

--- src/main/java/com/example/game/GameManager.java
+++ src/main/java/com/example/game/GameManager.java
@@ -28,7 +28,7 @@ public class GameManager extends Application {
     private double time;
 
 
-    public static final int TILE_SIZE = 40; // Grace's constant don't delete it.
+    public static final int TETRIUS_SIZE = 40; // Grace's constant don't delete it.
 
     public static final int GRID_WIDTH = 10;
     public static final int GRID_HEIGHT = 14;
